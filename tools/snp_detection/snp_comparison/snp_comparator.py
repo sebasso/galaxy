81175e50a923503cfa73c0ca5d60725e8f2abb71
sebasso <sebasso@student.matnat.uio.no>
2017-07-09 14:56:04 +0200

    snp_comparator added, this tool takes two snp_formatted_lists as input and produce statistics for the two tools and outputs it as json.

--- tools/snp_detection/snp_comparison/snp_comparator.py
+++ tools/snp_detection/snp_comparison/snp_comparator.py
@@ -3,7 +3,20 @@
 
 # usage: python snp_comparator.py comma separated list of SNP formatted files
 import sys
+import os
 from collections import Counter
+import json
+
+"""
+    further development:
+    1. create own for snp_formatted files so galaxy only provides those as options
+    - modify config/datatypes_conf.xml
+
+    x. make extensible for > 3 tool comparisons
+
+
+"""
+
 
 def readfiles(files):
     all_files = []
@@ -20,31 +33,38 @@ def find_SNPs_in_same_position(files):
 
     if len(files)>2:
         #multiple loop
-        print "doh"
+        print "not implemented yet"
+        exit(0)
     else:
         for lines in files[0]:
             for lines2 in files[1]:
                 l1 = lines.split("\t")
                 l2 = lines2.split("\t")
                 if  int(l1[1]) == int(l2[1]):
-                    print "success1"
                     SNPs_same_position.append(l1[0])
                     SNPs_same_position.append(l2[0])
                     if l1[0] == l2[0]:
-                        print "double success"
                         SNPs_same_position_and_file.append(l1[0])
                         SNPs_same_position_and_file.append(l2[0])
                 elif int(l1[1]) > int(l2[1]):
                     break
-    return (SNPs_same_position, SNPs_same_position_and_file)
 
+                return (SNPs_same_position, SNPs_same_position_and_file)
+
+
+def count_snps_per_genome(snp_lists_files):
+    SNPs_per_genome = []
+    for f in snp_lists_files:
+        genome_SNP_count = Counter(map(lambda x: x.split("\t")[0], f)) # dict response, should consider this on the count of position as well
+        #genome_loci_occurence = Counter(map(lambda x: x.split("\t")[1], f))
+        SNPs_per_genome.append(genome_SNP_count)
+
+    return SNPs_per_genome
 
 
 def compare_snps(SNP_files):
-    #files = SNP_files.split(",")
     files = SNP_files
-
-
+    #files = SNP_files
     numfiles = len(files)
 
     if numfiles <= 1:
@@ -53,42 +73,38 @@ def compare_snps(SNP_files):
 
     snp_lists_files = readfiles(files)
 
+    stats = {}
+    tool_names = []
+    for f in files:
+        tool_names.append(os.path.basename(f))
+
+    stats["tool_names"] = tool_names
+    stats["total_snps"] = []
+
+    #total number of snps per tool = len(all_files[0]), len(all_files[1]) ... len(all_files[x])
+    for i in xrange(0,len(snp_lists_files)):
+        stats["total_snps"].append(len(snp_lists_files[i]))
+
+    stats["SNPs_per_genome"] = count_snps_per_genome(snp_lists_files)
+    #stats["SNPs_loci_occurence"] = []
+
     SNPs_loci, SNPs_loci_filename = find_SNPs_in_same_position(snp_lists_files)
+    stats["same_loci_snps"] = SNPs_loci
+    stats["same_loci_filename"] = SNPs_loci_filename
 
+    r = json.dumps(stats)
 
+    for entry, value in stats.items():
+        print 'key: ',entry,'\t value: ',value
+
+    with open("snps_stats.json","w") as f:
+        f.write(r)
+
+    print "json stats: \n", r
     #SNPSs in total     #total number of snps per tool = len(all_files[0]), len(all_files[1]) ... len(all_files[x])
-
-    SNPs_per_genome = []
-    for f in snp_lists_files:
-
-        f = Counter(map(lambda x: x.split("\t")[0], f)) # dict response, should consider this on the count of position as well
-        print f
-        for a,b in f.items():
-            print a,b
-
-
     #collections.counter can easily find how many snps occur at one position, however not so relevant in comparison
-
-
-    #snps per genome
-
-    #snps with same loci
-
-
-
-     #total number of snps per tool = len(all_files[0]), len(all_files[1]) ... len(all_files[x])
-
-    #number_of_SNPs_per_genome()
-
-    #number_of_SNPs_in_total()
 
     #also mention files not present in one of the TOOLs, typically parsnp due to "core-genome"
 
-
-    # should consider sorting by filename and then position however may be harder to find SNP relation
-
 if __name__ == '__main__':
-    print sys.argv
-    #galaxy mode:
-    #sys.argv[1].split(",")
-    compare_snps(sys.argv[1:]) #TODO: change 1: to only 1 when production ready
+    compare_snps(sys.argv[1:])
